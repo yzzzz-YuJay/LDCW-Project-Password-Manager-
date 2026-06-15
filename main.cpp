976d802f1599e0fa9b54a9e2c79a9b6f39b93d06
TeeYuJay <teeyujay@gamil.com>
2026-06-15 12:22:48 +0800

    User Interface

--- main.cpp
+++ main.cpp
@@ -7,7 +7,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Smart Password Security Checker";
+    cout << "==================================================\n";
+    cout << "      SMART PASSWORD SECURITY CHECKER\n";
+    cout << "==================================================\n\n";
+
+    cout << "[1] Check Password\n\n";
+    cout << "[2] Password Security Tips\n\n";
+    cout << "[3] About Program\n\n";
+    cout << "[4] Exit Program\n\n";
+
+    cout << "==================================================\n";
     
     return 0;
 }
